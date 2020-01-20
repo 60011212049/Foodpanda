4d910fe2e37920c82b6ecbd94c1128783de1e3b2
Thanapol <55015794+60011212049@users.noreply.github.com>
2020-01-20 22:22:03 +0700

    testHeader

---
+++ indexDriver.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="CSSTopnav.css" title="style">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <title>FoodPanda - Login</title>
+    </head>
+    <body style="background-image: url('img/background1.jpg'); ">
+        <?php include 'header.php';?>
+
+        <div style="margin-top:3%"></div>
+
+        <script>
+            function myFunction() {
+                var x = document.getElementById("myTopnav");
+                if (x.className === "topnav") {
+                    x.className += " responsive";
+                } 
+                else {
+                    x.className = "topnav";
+                }
+            }
+        </script>
+        <center>
+            <img src="img/logo1.png" style="width:408px; margin-top:100px;">
+        </center>
+       
+                
+    </body>
+</html>
