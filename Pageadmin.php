b969aa3703422c9dc6d0f66f810d6ddb69f957d8
nicethanapol <49955977+nicethanapol@users.noreply.github.com>
2020-01-22 02:01:57 +0700

    Update Pageadmin.php

--- Pageadmin.php
+++ Pageadmin.php
@@ -1,19 +1,20 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <link rel="icon" type="image/png" href="images/icons/iconcsgo.png"/>
-        <link rel="stylesheet" type="text/css" href="CssBG.css" >
+        <link rel="stylesheet" type="text/css" href="CSSTopnav.css" title="style">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <title>admin</title>
-
-</style>
-    <body>
-         <table id="customers" style="margin-top: 10px">
-        <a  href="" style="float: right;"><button class ="button"  >จัดการรายชื่อร้านค้า</button></a>
-        <a  href="" style="float: right;"><button  class ="button" >จัดการรายชื่อคนขับ</button></a>
-        <a  href="" style="float: right;"><button  class ="button" >จัดการผู้ใช้</button></a>
-        <a  href="" style="float: right;"><button  class ="button" >ออกจากระบบ</button></a>
+    </head>
+    <body style="background-image: url('img/background1.jpg'); ">
+        <div class="topnav" id="myTopnav">
+            <p>foodpanda</p>
+            <a href="">ออกจากระบบ</a>
+            <a href="">จัดการผู้ใช้</a>
+            <a href="">จัดการรายชื่อคนขับ</a>
+            <a href="">จัดการรายชื่อร้านค้า</a>
+  
+        </div>
+        <div style="margin-top:3%"></div>
         
-
-        </table>
     </body>
 </html>
