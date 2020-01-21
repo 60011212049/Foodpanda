01990c8bf202703dfafb33d574bc5d895455531f
nicethanapol <49955977+nicethanapol@users.noreply.github.com>
2020-01-22 01:29:39 +0700

    Update Pageadmin.php

--- Pageadmin.php
+++ Pageadmin.php
@@ -1,54 +1,23 @@
 <html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="icon" type="image/png" href="images/icons/iconcsgo.png"/>
+        <link rel="stylesheet" type="text/css" href="CssBG.css" >
+        <title>admin</title>
 
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="CSSTopnav.css" title="style">
-    <link rel="stylesheet" type="text/css" href="CSSButton.css" title="style">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>FoodPanda - Login</title>
-    <style>
-        .clearfix {
-            overflow: auto;
-        }
-    </style>
-</head>
-
-<body style="background-image: url('img/background1.jpg'); ">
-    <?php include 'header.php'; ?>
-
-    <div style="margin-top:3%"></div>
-
-    <script>
-        function myFunction() {
-            var x = document.getElementById("myTopnav");
-            if (x.className === "topnav") {
-                x.className += " responsive";
-            } else {
-                x.className = "topnav";
-            }
-        }
-    </script>
-    <center>
-        <table>
-            <tr>
-                <div class="clearfix" style="border: 5px solid #cc0066;width: 50%;padding: 3px;">
-                    <img src="img/pro1.png" style="width:50%;float: right;">
-                    <!--<h3>Kito B.Duck BE4</h3>-->
-                    <!--<p class="price">388.- baht</p>-->
-                    <p><button>ดูรายละเอียดเพิ่มเติม</button></p>
-                </div>
-            </tr>
+</style>
+    <body>
+         <table id="customers" style="margin-top: 10px">
+        <button   >จัดการรายชื่อร้านค้า</button>
+        <button   >จัดการรายชื่อคนขับ</button>
+        <button   >จัดการผู้ใช้</button>
+        <button   >ออกจากระบบ</button>
+        
+       
+           
+           
+           
+            
         </table>
-
-
-
-    </center>
-    <script language="javascript" type="text/javascript">
-        function myFunction() {
-            window.location.href = "index.php";
-        }
-    </script>
-
-</body>
-
+    </body>
 </html>
